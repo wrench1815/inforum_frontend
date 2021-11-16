2892c9a61faabf0b0b0cbe8c82c9efb737fd11a5
coder-anmol <dev.anmolsharma@gmail.com>
2021-11-16 19:03:37 +0530

    git tested

---
+++ pages/anmol.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mt-7">
+    <h1 v-if="isHardeepNoob">Developed by @coder-anmol</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'anmol',
+  data() {
+    return {
+      isHardeepNoob: true,
+    }
+  },
+}
+</script>
+
+<style></style>
