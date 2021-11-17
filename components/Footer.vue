2b82aadbc263c9b62383b20bff18f6ebe36776e7
coder-anmol <dev.anmolsharma@gmail.com>
2021-11-17 21:50:06 +0530

    basic footer layout added

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,20 +1,78 @@
 <template>
   <!-- Footer -->
   <div>
-    <footer class="footer pt-9 pb-4">
+    <footer class="">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-3 mb-5 mb-lg-0">
-            <h6 class="text-uppercase mb-2 fs-5">Inforum</h6>
+        <!-- upper -->
+        <div class="row row-cols-lg-5 pt-4 pt-lg-5 my-5 border-top">
+          <div class="col-lg d-flex flex-column align-items-center">
+            <a href="#" class="d-flex align-items-center">
+              <p class="text-muted d-block text-2xl text-center text-lg-start">
+                <img src="../assets/icon.jpg" alt="logo" class="w-15 w-lg-25" />
+                Inforum
+              </p>
+            </a>
+            <a href="#" class="link-dark"
+              ><span class="badge bg-secondary mb-2">Email</span>
+              contact@inforum.com</a
+            >
+          </div>
+
+          <div class="col-lg"></div>
+
+          <div class="col-lg" :class="linksClasses">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Home</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">About</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Blog</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg" :class="linksClasses">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Home</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">About</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Blog</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg" :class="linksClasses">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Home</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">About</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Blog</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0 text-muted">Contact</a>
+              </li>
+            </ul>
           </div>
         </div>
-        <div class="d-flex justify-content-lg-center">
-          <hr class="horizontal dark border border-dark col-6" />
-        </div>
-        <div class="row">
-          <div class="col-8 mx-lg-auto text-lg-center">
-            <p class="text-md text-secondary">Copyright {{ year }}</p>
-          </div>
+
+        <!-- lower -->
+        <div class="row border-top">
+          <p class="text-center text-muted pt-3">© Inforum {{ year }}</p>
         </div>
       </div>
     </footer>
@@ -26,12 +84,11 @@
 export default {
   data() {
     return {
-      year,
+      year: new Date().getFullYear(),
+      linksClasses: `mt-3 mt-lg-0 d-flex flex-column align-items-center text-center text-lg-start`,
     }
   },
 }
-let currentTime = new Date()
-let year = currentTime.getFullYear()
 </script>
 
 <style scoped></style>
