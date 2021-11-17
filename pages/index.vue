99f19a72b370f61e17197bac540f32d115fcdaf0
Hardeep Kumar <hardeepkumar1815@gmail.com>
2021-11-17 22:38:46 +0530

    merge dev onto main (#3)
    * Add footer to site (#1)
    
    * git tested
    
    * test page removed
    
    * piro coder contact page done in 5 min
    
    * basic footer layout added
    
    Co-authored-by: coder-anmol <dev.anmolsharma@gmail.com>
    
    * update footer links
    
    * Add header to Index page (#2)
    
    * add test page
    
    * add header to index page
    add images
    
    * update navbar item active color
    
    Co-authored-by: coder-anmol <dev.anmolsharma@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,70 @@
 <template>
-  <div class="container mt-7">
-    <h1>This is Home</h1>
+  <div class="">
+    <!-- Header:Start -->
+    <header>
+      <div class="page-header min-vh-100">
+        <div
+          class="
+            w-lg-55 w-md-50 w-sm-100 w-100
+            position-absolute
+            fixed-top
+            ms-auto
+            h-100
+            z-index-0
+          "
+          style="background-image: url(/images/img-1.jpg)"
+          loading="lazy"
+        ></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-7">
+              <div class="card card-body blur shadow-lg p-5 my-5">
+                <h1>
+                  Welcome to
+                  <span class="text-gradient bg-gradient-primary">Inforum</span>
+                </h1>
+                <p class="lead text-bold">
+                  An Information Exchange for Students by Students.
+                </p>
+                <div class="buttons">
+                  <button type="button" class="btn bg-gradient-primary mt-4">
+                    Get Started
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- Header:End -->
+
+    <!-- Recent Forum Queries:Start -->
+    <div class="mx-3 mx-md-4">
+      <div class="container mt-6">
+        <div class="row pt-4">
+          <div class="col">
+            <h3>See Recent Queries</h3>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
+              dicta, beatae, cumque soluta quos deleniti nostrum quo iste ea ut
+              inventore illo labore consequatur laudantium quisquam praesentium
+              accusamus consequuntur temporibus!
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Recent Forum Queries:End -->
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+.header-bg-blur {
+  filter: blur(30px);
+}
+</style>
