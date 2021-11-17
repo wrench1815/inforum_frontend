e323a24fe36b2144ceb8341005221e8c27638ca1
Hardeep Kumar <hardeepkumar1815@gmail.com>
2021-11-17 22:24:30 +0530

    Add footer to site (#1)
    * git tested
    
    * test page removed
    
    * piro coder contact page done in 5 min
    
    * basic footer layout added
    
    Co-authored-by: coder-anmol <dev.anmolsharma@gmail.com>

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,45 @@
 <template>
-  <div class="container mt-7">
-    <h1>This is Contact Page</h1>
+  <div class="container mt-8">
+    <h1 class="text-center">Contact Us</h1>
+    <form>
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="input-group input-group-outline my-3">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-control" />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="input-group input-group-outline my-3">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control" />
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md">
+              <label class="form-label my-3">Message</label>
+              <div class="input-group input-group-outline">
+                <textarea
+                  class="form-control"
+                  placeholder="Leave your message"
+                  rows="15"
+                ></textarea>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md">
+              <button type="submit" class="btn bg-gradient-primary my-3">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
