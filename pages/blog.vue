5c5322b3e487ed6777bfc7cfab9caf91c1f0d725
coder-anmol <dev.anmolsharma@gmail.com>
2021-11-18 19:50:52 +0530

    test changes

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container mt-7">
     <h1>This is Blog Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ad.
+    </p>
   </div>
 </template>
 
