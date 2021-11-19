c8a78cfb729f4b19c396d27c989f1df16f63d91b
Hardeep Kumar <hardeepkumar1815@gmail.com>
2021-11-19 22:07:06 +0530

    fix issue with card image add layouts for cards

--- pages/blog.vue
+++ pages/blog.vue
@@ -3,49 +3,165 @@
     <!-- 01 Heading -->
     <h1>The Blog</h1>
 
-    <!-- 02 Featured Post -->
-    <div class="row">
-      <div class="col">
-        <div class="card my-4">
-          <div class="row g-0 height-400 overflow-hidden">
-            <div class="col-md-6 col-lg-7">
+    <!-- 02 Recent Posts -->
+
+    <div class="row g-4 mt-4">
+      <div class="col-12 mb-0 mb-sm-3">
+        <div class="row g-0">
+          <div class="col-sm-6 ps-sm-0 d-flex align-items-center">
+            <div class="card-image cursor-pointer h-100 w-100 ratio ratio-16x9">
               <img
-                src="../assets/demo-post.jpg"
-                class="img-fluid rounded-3 w-100 h-100 height-400 cover-img"
-                alt="demo-post"
+                class="
+                  shadow-lg
+                  border-radius-top-end-2xl
+                  border-radius-top-end-md-0
+                  border-radius-top-start-2xl
+                  border-radius-bottom-start-md-2xl
+                  img-fluid
+                  cover-img
+                "
+                src="@/assets/demo-post.jpg"
               />
             </div>
-            <div class="col-md-6 col-lg-5">
-              <div class="card-body">
-                <p class="card-text">
-                  <small class="text-muted">March 02, 2021</small>
-                </p>
-                <h5 class="card-title h2 pb-3">
-                  Learn Python In 30 Minutes - In Depth Guide
-                </h5>
-                <p class="card-text custom-paragraph">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Recusandae iste ex adipisci cumque architecto explicabo quas,
-                  dolores quibusdam sapiente fugit repellat expedita sint,
-                  maiores assumenda.iAnmi eos voluptatibus placeat nisi. This is
-                  a wider card with supporting text below as a natural lead-in
-                  to additional content. This content is a little bit longer.
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer. Lorem ipsum, dolor sit amet consectetur adipisicing
-                  elit. Voluptates ex animi earum ipsum quos esse, doloremque
-                  asperiores quasi tenetur praesentium temporibus, veniam
-                  dignissimos error quo autem libero officiis, beatae
-                  architecto.
-                </p>
-                <a href="#" class="btn btn-primary">Read More</a>
-              </div>
+          </div>
+          <div
+            class="
+              shadow-lg
+              border-radius-bottom-start-2xl
+              border-radius-bottom-start-md-0
+              border-radius-bottom-end-2xl
+              border-radius-top-end-md-2xl
+              border-radius-top-end-0
+              border-radius-top-start-0
+              border-radius-bottom-start-0
+              col-sm-6
+              ps-sm-0
+              card
+            "
+          >
+            <div class="card-body">
+              <p class="card-text">
+                <small class="text-muted">March 02, 2021</small>
+              </p>
+              <h3 class="card-title pb-3">
+                Learn Python In 30 Minutes - In Depth Guide
+              </h3>
+              <p class="card-text custom-paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Recusandae iste ex adipisci cumque architecto explicabo quas,
+                dolores quibusdam sapiente fugit repellat expedita sint, maiores
+                assumenda.iAnmi eos voluptatibus placeat nisi. This is a wider
+                card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer. This is
+                a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer. Lorem
+                ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
+                ex animi earum ipsum quos esse, doloremque asperiores quasi
+                tenetur praesentium temporibus, veniam dignissimos error quo
+                autem libero officiis, beatae architecto.
+              </p>
+              <a href="#" class="btn btn-primary">Read More</a>
             </div>
           </div>
         </div>
       </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-image">
+            <img class="img-fluid" src="/images/img-1.jpg" alt="" />
+          </div>
+          <div class="card-body">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit quaerat qui voluptatem modi ab, minus quibusdam iste
+              libero rem neque voluptatibus nisi ea accusantium, totam
+              voluptatum. Totam tempora commodi officiis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-image">
+            <img class="img-fluid" src="/images/img-1.jpg" alt="" />
+          </div>
+          <div class="card-body">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit quaerat qui voluptatem modi ab, minus quibusdam iste
+              libero rem neque voluptatibus nisi ea accusantium, totam
+              voluptatum. Totam tempora commodi officiis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-image">
+            <img class="img-fluid" src="/images/img-1.jpg" alt="" />
+          </div>
+          <div class="card-body">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit quaerat qui voluptatem modi ab, minus quibusdam iste
+              libero rem neque voluptatibus nisi ea accusantium, totam
+              voluptatum. Totam tempora commodi officiis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-image">
+            <img class="img-fluid" src="/images/img-1.jpg" alt="" />
+          </div>
+          <div class="card-body">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit quaerat qui voluptatem modi ab, minus quibusdam iste
+              libero rem neque voluptatibus nisi ea accusantium, totam
+              voluptatum. Totam tempora commodi officiis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-image">
+            <img class="img-fluid" src="/images/img-1.jpg" alt="" />
+          </div>
+          <div class="card-body">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit quaerat qui voluptatem modi ab, minus quibusdam iste
+              libero rem neque voluptatibus nisi ea accusantium, totam
+              voluptatum. Totam tempora commodi officiis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-image">
+            <img class="img-fluid" src="/images/img-1.jpg" alt="" />
+          </div>
+          <div class="card-body">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit quaerat qui voluptatem modi ab, minus quibusdam iste
+              libero rem neque voluptatibus nisi ea accusantium, totam
+              voluptatum. Totam tempora commodi officiis.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-    <!-- 03 Recent Posts -->
   </div>
 </template>
 
