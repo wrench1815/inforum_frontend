84c2f34d814a92479951be6a35af7f6a2e633ae7
Hardeep Kumar <hardeepkumar1815@gmail.com>
2021-12-01 15:10:38 +0530

    update table layout

--- pages/admin/blogpost/index.vue
+++ pages/admin/blogpost/index.vue
@@ -4,7 +4,7 @@
     <div class="container-fluid py-4">
       <div class="row">
         <div class="col-12">
-          <h1>Blog Post Admin</h1>
+          <h2>Blog Posts</h2>
         </div>
 
         <div class="col-12" v-if="blogPosts.length == 0">
@@ -16,83 +16,44 @@
             <div class="table-responsive">
               <table class="table align-items-center mb-0">
                 <thead>
-                  <tr>
-                    <th
-                      class="
-                        text-uppercase text-secondary text-xxs
-                        font-weight-bolder
-                        opacity-7
-                      "
-                    >
-                      id
-                    </th>
-                    <th
-                      class="
-                        text-uppercase text-secondary text-xxs
-                        font-weight-bolder
-                        opacity-7
-                        ps-2
-                      "
-                    >
-                      Title
-                    </th>
-                    <th
-                      class="
-                        text-center text-uppercase text-secondary text-xxs
-                        font-weight-bolder
-                        opacity-7
-                      "
-                    >
-                      Author
-                    </th>
-                    <th
-                      class="
-                        text-center text-uppercase text-secondary text-xxs
-                        font-weight-bolder
-                        opacity-7
-                      "
-                    >
-                      Date Posted
-                    </th>
-                    <th
-                      class="
-                        text-center text-uppercase text-secondary text-xxs
-                        font-weight-bolder
-                        opacity-7
-                      "
-                    >
-                      Actions
-                    </th>
+                  <tr class="text-primary text-center text-md">
+                    <th class="text-uppercase">id</th>
+                    <th class="text-uppercase text-start ps-2">Title</th>
+                    <th class="text-uppercase">Author</th>
+                    <th class="text-uppercase">Date Posted</th>
+                    <th class="text-uppercase">Actions</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="post in blogPosts" :key="post.id">
-                    <td class="align-middle text-center text-sm">
-                      <p class="text-xs font-weight-bold mb-0">{{ post.id }}</p>
+                  <tr
+                    class="align-middle text-center text-dark text-"
+                    v-for="post in blogPosts"
+                    :key="post.id"
+                  >
+                    <td class="text-bold">
+                      {{ post.id }}
                     </td>
-                    <td>
-                      <p class="text-xs font-weight-bold mb-0 text-capitalize">
-                        {{ post.title }}
-                      </p>
+                    <td
+                      class="
+                        text-capitalize text-start
+                        link-info
+                        cursor-pointer
+                      "
+                    >
+                      {{ post.title }}
                     </td>
-                    <td>
-                      <p class="text-xs text-center font-weight-bold mb-0">
-                        Hardeep Kumar
-                      </p>
-                    </td>
+                    <td class="text-capitalize">Hardeep Kumar</td>
 
-                    <td class="align-middle text-center">
-                      <span class="text-secondary text-xs font-weight-normal">
-                        {{ formattedDate(post.datePosted) }}
-                      </span>
+                    <td class="text-center">
+                      {{ formattedDate(post.datePosted) }}
                     </td>
-                    <td class="align-middle">
-                      <div class="d-flex justify-content-around">
+                    <td>
+                      <div class="d-flex justify-content-evenly">
                         <a>
-                          <i class="fas fa-edit"></i>
+                          <i class="fas fa-edit text-success"></i>
                         </a>
                         <a>
-                          <i class="fas fa-trash"></i>
+                          <i class="fas fa-trash text-danger"></i>
                         </a>
                       </div>
                     </td>
@@ -117,7 +78,7 @@ export default {
   methods: {
     formattedDate(inputDate) {
       const myDate = new Date(inputDate)
-      return `${myDate.getDate()}-${myDate.getMonth()}-${myDate.getFullYear()}`
+      return `${myDate.getDay()}, ${myDate.getDate()}-${myDate.getMonth()}-${myDate.getFullYear()}`
     },
   },
 }
