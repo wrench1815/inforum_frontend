93575e4c32501e5a63d733a4922927911ce08d08
Hardeep Kumar <hardeepkumar1815@gmail.com>
2021-12-01 15:38:11 +0530

    update page layout

--- pages/admin/blogpost/index.vue
+++ pages/admin/blogpost/index.vue
@@ -2,64 +2,66 @@
   <div>
     <AdminNavBar />
     <div class="container-fluid py-4">
-      <div class="row">
-        <div class="col-12">
-          <h2>Blog Posts</h2>
-        </div>
+      <div class="card py-4">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="mx-4">Blog Posts</h2>
+          </div>
 
-        <div class="col-12" v-if="blogPosts.length == 0">
-          <h1>No posts available</h1>
-        </div>
-        <!-- create fking table: id , title,author,dateposted,actions:(icons: edit,delete) -->
-        <div class="col-12" v-else>
-          <div class="card">
-            <div class="table-responsive">
-              <table class="table align-items-center mb-0">
-                <thead>
-                  <tr class="text-primary text-center text-md">
-                    <th class="text-uppercase">id</th>
-                    <th class="text-uppercase text-start ps-2">Title</th>
-                    <th class="text-uppercase">Author</th>
-                    <th class="text-uppercase">Date Posted</th>
-                    <th class="text-uppercase">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    class="align-middle text-center text-dark text-"
-                    v-for="post in blogPosts"
-                    :key="post.id"
-                  >
-                    <td class="text-bold">
-                      {{ post.id }}
-                    </td>
-                    <td
-                      class="
-                        text-capitalize text-start
-                        link-info
-                        cursor-pointer
-                      "
+          <div class="col-12" v-if="blogPosts.length == 0">
+            <h1>No posts available</h1>
+          </div>
+          <!-- create fking table: id , title,author,dateposted,actions:(icons: edit,delete) -->
+          <div class="col-12" v-else>
+            <div class="">
+              <div class="table-responsive">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr class="text-primary text-center text-md">
+                      <th class="text-uppercase">id</th>
+                      <th class="text-uppercase text-start ps-2">Title</th>
+                      <th class="text-uppercase">Author</th>
+                      <th class="text-uppercase">Date Posted</th>
+                      <th class="text-uppercase">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      class="align-middle text-center text-dark text-"
+                      v-for="post in blogPosts"
+                      :key="post.id"
                     >
-                      {{ post.title }}
-                    </td>
-                    <td class="text-capitalize">Hardeep Kumar</td>
+                      <td class="text-bold">
+                        {{ post.id }}
+                      </td>
+                      <td
+                        class="
+                          text-capitalize text-start
+                          link-info
+                          cursor-pointer
+                        "
+                      >
+                        {{ post.title }}
+                      </td>
+                      <td class="text-capitalize">Hardeep Kumar</td>
 
-                    <td class="text-center">
-                      {{ formattedDate(post.datePosted) }}
-                    </td>
-                    <td>
-                      <div class="d-flex justify-content-evenly">
-                        <a>
-                          <i class="fas fa-edit text-success"></i>
-                        </a>
-                        <a>
-                          <i class="fas fa-trash text-danger"></i>
-                        </a>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+                      <td class="text-center">
+                        {{ formattedDate(post.datePosted) }}
+                      </td>
+                      <td>
+                        <div class="d-flex justify-content-evenly">
+                          <a>
+                            <i class="fas fa-edit text-success"></i>
+                          </a>
+                          <a>
+                            <i class="fas fa-trash text-danger"></i>
+                          </a>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
