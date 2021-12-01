ce104aca4ce3ba5ce83ac9e59dbb2e729fbaa17b
Anmol Sharma <64188575+coder-anmol@users.noreply.github.com>
2021-12-01 20:50:46 +0530

    Admin home (#11)
    * blog-post table created
    
    * update table layout
    
    * update page layout
    
    * wednesday added
    
    * minor update in layout
    
    * links nav added and basic crud pages created
    
    * dumb forms added, i know its not good
    
    * fix formatting
    
    * load categories from api
    
    * add button for adding new Post
    
    * update creation form
    
    add api connection to POST data for post
    
    Co-authored-by: Hardeep Kumar <hardeepkumar1815@gmail.com>

---
+++ pages/admin/blogpost/add.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <AdminNavBar />
+    <div class="container-fluid py-4">
+      <div class="card py-4">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="mx-4">Add Post</h2>
+          </div>
+
+          <!-- Start:Post Add Form -->
+          <div class="col-12">
+            <div class="card-body position-relative">
+              <form v-on:submit.prevent="addPost">
+                <div class="row mt-4">
+                  <!-- Start:Title -->
+                  <div class="col-12">
+                    <div class="input-group input-group-static my-4">
+                      <label class="text-primary">Title</label>
+                      <input
+                        class="form-control"
+                        type="text"
+                        v-model="postTitle"
+                      />
+                    </div>
+                  </div>
+
+                  <!-- End:Title -->
+
+                  <!-- Start:Content -->
+                  <div class="col-12">
+                    <div class="input-group input-group-static mt-4">
+                      <label class="text-primary">Content</label>
+                      <textarea
+                        name="content"
+                        class="form-control"
+                        rows="10"
+                        v-model="postContent"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <!-- End:Title -->
+
+                  <!-- Start:Excerpt -->
+                  <div class="col-12">
+                    <div class="input-group input-group-static mt-4">
+                      <label class="text-primary">Excerpt</label>
+                      <textarea
+                        name="exerpt"
+                        class="form-control"
+                        rows="4"
+                        v-model="postExcerpt"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <!-- End:Excerpt -->
+
+                  <!-- Start:Category -->
+                  <div class="col">
+                    <div class="input-group input-group-static my-4">
+                      <label class="text-primary">Category</label>
+                      <select
+                        class="form-select form-control w-100 ps-3 mt-3"
+                        v-model="postCategory"
+                      >
+                        <option disabled>Select Post Category</option>
+                        <option
+                          v-for="cat in categories"
+                          :key="cat.id"
+                          v-bind:value="cat.id"
+                        >
+                          {{ cat.name }}
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+                  <!-- End:Category -->
+                </div>
+                <!-- Start:Submit Button -->
+                <div class="text-end mt-4">
+                  <button type="submit" class="btn bg-gradient-primary mb-0">
+                    Add Post
+                  </button>
+                </div>
+                <!-- Start:Submit Button -->
+              </form>
+            </div>
+          </div>
+          <!-- End:Post Add Form -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  data() {
+    return {
+      postTitle: '',
+      postContent: '',
+      postExcerpt: '',
+      postCategory: '',
+    }
+  },
+
+  async asyncData({ $axios, $config }) {
+    const categories = await $axios.$get('/Categories')
+    return { categories }
+  },
+
+  methods: {
+    async addPost() {
+      const data = {
+        title: this.postTitle,
+        description: this.postContent,
+        excerpt: this.postExcerpt,
+        slug: 'test-nuxt',
+        categoryId: parseInt(this.postCategory),
+      }
+      await this.$axios.$post('/BlogPosts', data)
+      this.$router.push('/admin/blogpost')
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/admin/blogpost/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <AdminNavBar />
+
+    <!-- content -->
+    <div class="container-fluid py-4">
+      <div class="card py-4">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="mx-4">Blog Posts</h2>
+          </div>
+          <div class="col-12" v-if="blogPosts.length == 0">
+            <h1>No posts available</h1>
+          </div>
+          <!-- create fking table: id , title,author,dateposted,actions:(icons: edit,delete) -->
+          <div class="col-12" v-else>
+            <div class="mx-4">
+              <div class="table-responsive">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr class="text-primary text-center text-md">
+                      <th class="text-uppercase">id</th>
+                      <th class="text-uppercase text-start ps-2">Title</th>
+                      <th class="text-uppercase">Author</th>
+                      <th class="text-uppercase">Date Posted</th>
+                      <th class="text-uppercase">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      class="align-middle text-center text-dark text-"
+                      v-for="post in blogPosts"
+                      :key="post.id"
+                    >
+                      <td class="text-bold">
+                        {{ post.id }}
+                      </td>
+                      <td
+                        class="
+                          text-capitalize text-start
+                          link-info
+                          cursor-pointer
+                        "
+                      >
+                        {{ post.title }}
+                      </td>
+                      <td class="text-capitalize">Hardeep Kumar</td>
+
+                      <td class="text-center">
+                        {{ formattedDate(post.datePosted) }}
+                      </td>
+                      <td>
+                        <div class="d-flex justify-content-evenly">
+                          <a>
+                            <i class="fas fa-edit text-warning"></i>
+                          </a>
+                          <a>
+                            <i class="fas fa-trash text-danger"></i>
+                          </a>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="d-flex justify-content-end mx-4 mt-4">
+            <NuxtLink class="btn btn-success" to="/admin/blogpost/add"
+              >Add Post</NuxtLink
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  async asyncData({ $axios, $config }) {
+    const blogPosts = await $axios.$get('/BlogPosts')
+    return { blogPosts }
+  },
+
+  methods: {
+    formattedDate(inputDate) {
+      const myDate = new Date(inputDate)
+      return `${myDate.toLocaleString('default', {
+        weekday: 'short',
+      })}, ${myDate.getDate()}-${myDate.getMonth()}-${myDate.getFullYear()}`
+    },
+  },
+}
+</script>
+
+<style></style>
