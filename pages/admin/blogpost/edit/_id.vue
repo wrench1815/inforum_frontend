eb43347393d910a4bccdb8525e3fe2653cb96406
Hardeep Kumar <hardeepkumar1815@gmail.com>
2021-12-02 21:01:28 +0530

    dynamic route for edit

---
+++ pages/admin/blogpost/edit/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <AdminNavBar />
+
+    <!-- Start:content -->
+    <div class="container-fluid py-4">
+      <div class="card py-4">
+        <div class="row">
+          <div class="col-12">
+            <p>{{ blogPost }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End:content -->
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  async asyncData({ $axios, $config, params }) {
+    const blogPost = await $axios.$get(`/BlogPosts/${params.id}`)
+    return { blogPost }
+  },
+}
+</script>
+
+<style></style>
