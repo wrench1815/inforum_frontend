4cd4506b71945472084e815c67852208690f04a7
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-07 16:57:57 +0530

    complete home  with validations and modals (#19)
    Co-authored-by: Anmol Sharma <dev.anmolsharma@gmail.com>

---
+++ pages/admin/home/preview/_id.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <AdminNavBar />
+
+    <!-- Start:content -->
+    <div class="container-fluid py-4">
+      <div class="card py-4">
+        <div class="row">
+          <div class="col-12">
+            <h1 class="mx-4 mb-3">Preview Home</h1>
+          </div>
+          <div class="col-12">
+            <div class="mx-4 mb-5">
+              <span class="mb-2 badge rounded-pill bg-primary">Heading</span>
+              <h5 class="text-capitalize">{{ homeData.heading }}</h5>
+            </div>
+            <div class="mx-4 mb-5">
+              <span class="mb-2 badge rounded-pill bg-primary"
+                >Sub Heading</span
+              >
+              <h5 class="text-capitalize">{{ homeData.subHeading }}</h5>
+            </div>
+            <div class="mx-4">
+              <span class="mb-3 badge rounded-pill bg-primary"
+                >Header Image</span
+              >
+              <br />
+              <img
+                class="img-fluid"
+                :src="homeData.headerImageLink"
+                alt="header image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End:content -->
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  async asyncData({ $axios, $config, params }) {
+    const homeData = await $axios.$get(`/Home/${params.id}`)
+    return { homeData }
+  },
+}
+</script>
+
+<style></style>
