5801e076a5e9424ba60524c049e9caf4b30887e5
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-18 22:47:53 +0530

    add Post Card Component

---
+++ components/Blog/PostCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card border-radius-2xl">
+    <div
+      class="card-image border-radius-top-start-2xl border-radius-top-end-2xl cursor-pointer img-hover-zoom"
+    >
+      <img
+        class="img-fluid border-radius-top-start-2xl border-radius-top-end-2xl"
+        src="@/assets/images/img-1.jpg"
+        alt=""
+      />
+    </div>
+    <div class="card-body">
+      <NuxtLink class="card-title h3" to="/">{{ post.title }}</NuxtLink>
+      <p class="custom-paragraph">
+        {{ post.excerpt }}
+      </p>
+      <a href="#" class="btn btn-primary">Read More</a>
+      <div class="border-top border-primary mb-1"></div>
+      <p class="card-text d-flex justify-content-between">
+        <small class="text-info">March 02, 2021</small>
+        <small class="text-info">John Doe</small>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostCard',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
