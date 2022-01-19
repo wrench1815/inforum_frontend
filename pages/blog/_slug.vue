4d64a739060eb91e0cd1a31cc4f72ebff7b0dd85
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-19 15:07:48 +0530

    update blog routing for dynamic use

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mt-7">
+    {{ post }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: '',
+    }
+  },
+  mounted() {
+    const post = this.$axios.$get(`/BlogPosts/${this.$route.params.slug}`)
+    post.then((res) => {
+      this.post = res
+    })
+  },
+}
+</script>
+
+<style scoped></style>
