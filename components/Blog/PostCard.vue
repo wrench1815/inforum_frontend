2c958c0273cdc60e9b3b6d6f087e24e84fdbbb57
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-19 15:10:42 +0530

    update card to route to single posts

--- components/Blog/PostCard.vue
+++ components/Blog/PostCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="card border-radius-2xl">
-    <div
+    <NuxtLink
+      :to="`/blog/${post.slug}`"
       class="card-image border-radius-top-start-2xl border-radius-top-end-2xl cursor-pointer img-hover-zoom"
     >
       <img
@@ -8,13 +9,17 @@
         src="@/assets/images/img-1.jpg"
         alt=""
       />
-    </div>
+    </NuxtLink>
     <div class="card-body">
-      <NuxtLink class="card-title h3" to="/">{{ post.title }}</NuxtLink>
+      <NuxtLink class="card-title h3" :to="`/blog/${post.slug}`">{{
+        post.title
+      }}</NuxtLink>
       <p class="custom-paragraph">
         {{ post.excerpt }}
       </p>
-      <a href="#" class="btn btn-primary">Read More</a>
+      <NuxtLink :to="`/blog/${post.slug}`" class="btn btn-primary"
+        >Read More</NuxtLink
+      >
       <div class="border-top border-primary mb-1"></div>
       <p class="card-text d-flex justify-content-between">
         <small class="text-info">March 02, 2021</small>
