ceb555aac4115d722757d8c0ba584b6d0b089d7b
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-19 18:01:05 +0530

    fix component for linking to single post

--- components/Blog/FullPostCard.vue
+++ components/Blog/FullPostCard.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="row g-0">
     <div class="col-sm-6 ps-sm-0 d-flex align-items-center">
-      <div
+      <NuxtLink
+        :to="`/blog/${post.slug}`"
         class="card-image cursor-pointer h-100 w-100 ratio ratio-16x9 border-radius-top-end-2xl border-radius-top-end-md-0 border-radius-top-start-2xl border-radius-bottom-start-md-2xl img-hover-zoom"
       >
         <img class="cover-img" src="@/assets/images/img-1.jpg" />
-      </div>
+      </NuxtLink>
     </div>
     <div
       class="shadow-lg border-radius-bottom-start-2xl border-radius-bottom-start-md-0 border-radius-bottom-end-2xl border-radius-top-end-md-2xl border-radius-top-end-0 border-radius-top-start-0 border-radius-bottom-start-0 col-sm-6 ps-sm-0 card"
@@ -16,7 +17,7 @@
           <span class="text-primary">|</span>
           <span class="text-info"> John Doe</span>
         </p>
-        <NuxtLink class="card-title h3 pb-3" to="/"
+        <NuxtLink class="card-title h3 pb-3" :to="`/blog/${post.slug}`"
           >Learn Python In 30 Minutes - In Depth Guide</NuxtLink
         >
         <p class="card-text custom-paragraph">
@@ -32,14 +33,24 @@
           tenetur praesentium temporibus, veniam dignissimos error quo autem
           libero officiis, beatae architecto.
         </p>
-        <a href="#" class="btn btn-primary">Read More</a>
+        <NuxtLink :to="`/blog/${post.slug}`" class="btn btn-primary"
+          >Read More</NuxtLink
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'FullPostCard',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+}
 </script>
 
-<style></style>
+<style scoped></style>
