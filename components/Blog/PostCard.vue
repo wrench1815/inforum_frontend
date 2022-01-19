14f46de25f2af6a0169aea4ec09071b33ee56306
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-19 17:32:30 +0530

    fix link connections to single Post

--- components/Blog/PostCard.vue
+++ components/Blog/PostCard.vue
@@ -4,11 +4,7 @@
       :to="`/blog/${post.slug}`"
       class="card-image border-radius-top-start-2xl border-radius-top-end-2xl cursor-pointer img-hover-zoom"
     >
-      <img
-        class="img-fluid border-radius-top-start-2xl border-radius-top-end-2xl"
-        src="@/assets/images/img-1.jpg"
-        alt=""
-      />
+      <img class="img-fluid" src="@/assets/images/img-1.jpg" alt="" />
     </NuxtLink>
     <div class="card-body">
       <NuxtLink class="card-title h3" :to="`/blog/${post.slug}`">{{
