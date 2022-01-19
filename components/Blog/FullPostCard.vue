58c3c3dcf966f7ebe2d1a36dcf184e037ffd1a6b
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-19 17:32:08 +0530

    Transfer Code to Component

---
+++ components/Blog/FullPostCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row g-0">
+    <div class="col-sm-6 ps-sm-0 d-flex align-items-center">
+      <div
+        class="card-image cursor-pointer h-100 w-100 ratio ratio-16x9 border-radius-top-end-2xl border-radius-top-end-md-0 border-radius-top-start-2xl border-radius-bottom-start-md-2xl img-hover-zoom"
+      >
+        <img class="cover-img" src="@/assets/images/img-1.jpg" />
+      </div>
+    </div>
+    <div
+      class="shadow-lg border-radius-bottom-start-2xl border-radius-bottom-start-md-0 border-radius-bottom-end-2xl border-radius-top-end-md-2xl border-radius-top-end-0 border-radius-top-start-0 border-radius-bottom-start-0 col-sm-6 ps-sm-0 card"
+    >
+      <div class="card-body">
+        <p class="card-text">
+          <small class="text-info">March 02, 2021</small>
+          <span class="text-primary">|</span>
+          <span class="text-info"> John Doe</span>
+        </p>
+        <NuxtLink class="card-title h3 pb-3" to="/"
+          >Learn Python In 30 Minutes - In Depth Guide</NuxtLink
+        >
+        <p class="card-text custom-paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          iste ex adipisci cumque architecto explicabo quas, dolores quibusdam
+          sapiente fugit repellat expedita sint, maiores assumenda.iAnmi eos
+          voluptatibus placeat nisi. This is a wider card with supporting text
+          below as a natural lead-in to additional content. This content is a
+          little bit longer. This is a wider card with supporting text below as
+          a natural lead-in to additional content. This content is a little bit
+          longer. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Voluptates ex animi earum ipsum quos esse, doloremque asperiores quasi
+          tenetur praesentium temporibus, veniam dignissimos error quo autem
+          libero officiis, beatae architecto.
+        </p>
+        <a href="#" class="btn btn-primary">Read More</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
