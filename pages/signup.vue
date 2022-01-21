672a14f7dcf76e00fcf6bb18a2e0ec4ce299e12e
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-21 15:46:05 +0530

    fix resizing issue fixed on validation check
    add helptext explaining form purpose

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,51 +1,41 @@
 <template>
-  <div class="page-header">
-    <div class="position-fixed fixed-top w-100 h-100 z-index-0 header-img">
-      <!-- Start:BG Overlay Mask -->
-      <div
-        class="position-absolute start-0 top-0 h-100 z-index-1 opacity-6 w-100 bg-gradient-dark"
-      >
-        <LazyParticles
-          v-if="loadParticles"
-          class="w-100 h-100 opacity-100"
-          :lineLinked="false"
-          :particleOpacity="0.5"
-          movementDirection="none"
-          out_mode="bounce"
-        ></LazyParticles>
-      </div>
-      <!-- End:BG Overlay Mask -->
-    </div>
-    <div class="container py-9">
+  <div
+    class="page-header align-items-start min-vh-100 header-img"
+    loading="lazy"
+  >
+    <span class="mask bg-gradient-dark opacity-6">
+      <LazyParticles
+        v-if="loadParticles"
+        class="w-100 h-100 opacity-100"
+        :lineLinked="false"
+        :particleOpacity="0.5"
+        movementDirection="none"
+        out_mode="bounce"
+      ></LazyParticles>
+    </span>
+    <div class="container mt-9 mb-4">
       <div class="row">
-        <div
-          class="col-lg-6 col-md-10 mx-auto d-flex justify-content-center align-items-center flex-column"
-        >
-          <div
-            class="card card-body d-flex justify-content-center shadow-lg pt-1 blur align-items-center"
-          >
-            <div
-              class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 w-100 mb-3"
-            >
+        <div class="col-lg-7 col-md-7 col-12 mx-auto">
+          <div class="card z-index-0">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
               <div
                 class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1"
               >
-                <h3 class="text-white text-center mt-2 mb-0">Sign Up</h3>
+                <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">
+                  Sign Up
+                </h4>
+                <p class="text-white font-weight-bolder text-center mt-2 mb-0">
+                  Enter Valid Information Below to Sign Up
+                </p>
               </div>
             </div>
-
-            <!-- Start:Signup Form -->
-            <form
-              role="form"
-              class="px-auto"
-              @submit="signup"
-              autocomplete="off"
-            >
-              <div class="card-body">
+            <div class="card-body">
+              <!-- Start:Sign Up Form -->
+              <form role="form" class="text-start" @submit="signup">
                 <div class="row">
                   <div class="col-md-6">
                     <!-- Start:First Name -->
-                    <div class="input-group input-group-static my-4">
+                    <div class="input-group input-group-static mt-3">
                       <label class="text-primary">First Name</label>
                       <input
                         type="text"
@@ -54,20 +44,22 @@
                         v-model="firstName"
                         @keyup="validateFirstName"
                       />
-                      <!-- Validation -->
-                      <div
-                        class="text-sm mt-2 text-danger text-bold"
-                        v-if="FormHelpTexts.firstNameText"
-                      >
-                        {{ FormHelpTexts.firstNameText }}
-                      </div>
                     </div>
                     <!-- End:First Name -->
+
+                    <!-- Start:First Name Validation -->
+                    <div
+                      class="text-sm mt-2 text-danger text-bold"
+                      v-if="FormHelpTexts.firstNameText"
+                    >
+                      {{ FormHelpTexts.firstNameText }}
+                    </div>
+                    <!-- End:First Name Validation -->
                   </div>
 
                   <div class="col-md-6">
                     <!-- Start:Last Name -->
-                    <div class="input-group input-group-static my-4">
+                    <div class="input-group input-group-static mt-3">
                       <label class="text-primary">Last Name</label>
                       <input
                         type="text"
@@ -76,136 +68,135 @@
                         v-model="lastName"
                         @keyup="validateLastName"
                       />
-                      <!-- Validation -->
-                      <div
-                        class="text-sm mt-2 text-danger text-bold"
-                        v-if="FormHelpTexts.lastNameText"
-                      >
-                        {{ FormHelpTexts.lastNameText }}
-                      </div>
                     </div>
                     <!-- End:Last Name -->
-                  </div>
 
-                  <div class="col-12">
-                    <!-- Start:Email -->
-                    <div class="input-group input-group-static my-4">
-                      <label class="text-primary">Email</label>
-                      <input
-                        type="email"
-                        class="form-control"
-                        placeholder="johndoe@mail.com"
-                        v-model="email"
-                        @keyup="validateEmail"
-                      />
-                      <!-- Validation -->
-                      <div
-                        class="text-danger text-sm mt-2 text-bold"
-                        v-if="FormHelpTexts.emailText"
-                      >
-                        {{ FormHelpTexts.emailText }}
-                      </div>
-                    </div>
-                    <!-- End:Email -->
-                  </div>
-
-                  <div class="col-12">
-                    <!-- Start:Gender -->
-                    <div class="input-group input-group-static my-4">
-                      <label class="text-primary">Gender</label>
-                      <select
-                        class="form-select form-control w-100 ps-3 mt-3"
-                        v-model="gender"
-                        @change="validateGender"
-                      >
-                        <option disabled value="">Select Your Gender</option>
-                        <option value="0">Male</option>
-                        <option value="1">Female</option>
-                        <option value="2">Unspecified</option>
-                      </select>
-                      <!-- Validation -->
-                      <div
-                        class="text-sm mt-2 text-danger text-bold"
-                        v-if="FormHelpTexts.genderText"
-                      >
-                        {{ FormHelpTexts.genderText }}
-                      </div>
-                    </div>
-                    <!-- End:Gender -->
-                  </div>
-
-                  <div class="col-12">
-                    <!-- Start:Password -->
-                    <div class="input-group input-group-static my-4">
-                      <label class="text-primary">Password</label>
-                      <input
-                        type="password"
-                        class="form-control"
-                        v-model="password"
-                        placeholder="********"
-                        @keyup="validatePassword"
-                      />
-                      <!-- Validation -->
-                      <div
-                        class="text-danger text-sm mt-2 text-bold"
-                        v-if="FormHelpTexts.passwordText"
-                      >
-                        {{ FormHelpTexts.passwordText }}
-                      </div>
-                    </div>
-                    <!-- End:Password -->
-                  </div>
-
-                  <div class="col-12">
-                    <!-- Start:Confirm Password -->
-                    <div class="input-group input-group-static my-4">
-                      <label class="text-primary">Confirm Password</label>
-                      <input
-                        type="password"
-                        class="form-control"
-                        v-model="confirmPassword"
-                        placeholder="********"
-                        @keyup="validateConfirmPassword"
-                      />
-                      <!-- Validation -->
-                      <div
-                        class="text-danger text-sm mt-2 text-bold"
-                        v-if="FormHelpTexts.confirmPasswordText"
-                      >
-                        {{ FormHelpTexts.confirmPasswordText }}
-                      </div>
-                    </div>
-                    <!-- End:Confirm Password -->
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-12">
-                    <!-- Start:Action Button -->
-                    <button
-                      type="submit"
-                      class="btn bg-gradient-primary w-100 my-4 mb-2"
+                    <!-- Start:Last Name Validation -->
+                    <div
+                      class="text-sm mt-2 text-danger text-bold"
+                      v-if="FormHelpTexts.lastNameText"
                     >
-                      Sign up
-                    </button>
-                    <!-- End:Action Button -->
-                  </div>
-
-                  <div class="col-12">
-                    <!-- Start:Login Link -->
-                    <p class="mt-4 text-sm text-center">
-                      Already have an account?
-                      <NuxtLink
-                        class="text-primary text-gradient font-weight-bold"
-                        to="/login"
-                        >Login</NuxtLink
-                      >
-                    </p>
-                    <!-- End:Login Link -->
+                      {{ FormHelpTexts.lastNameText }}
+                    </div>
+                    <!-- End:Last Name Validation -->
                   </div>
                 </div>
-              </div>
-            </form>
-            <!-- End:Signup Form -->
+
+                <!-- Start:Email -->
+                <div class="input-group input-group-static mt-3">
+                  <label class="text-primary">Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    placeholder="person@mail.com"
+                    v-model="email"
+                    @keyup="validateEmail"
+                  />
+                </div>
+                <!-- End:Email -->
+
+                <!-- Start:Email Validation -->
+                <div
+                  class="text-danger text-sm mt-2 text-bold"
+                  v-if="FormHelpTexts.emailText"
+                >
+                  {{ FormHelpTexts.emailText }}
+                </div>
+                <!-- End:Email Validation -->
+
+                <!-- Start:Gender -->
+                <div class="input-group input-group-static mt-3">
+                  <label class="text-primary">Gender</label>
+                  <select
+                    class="form-select form-control"
+                    v-model="gender"
+                    @change="validateGender"
+                  >
+                    <option disabled value="">Select your Gender</option>
+                    <option value="0">Male</option>
+                    <option value="1">Female</option>
+                    <option value="2">Unspecified</option>
+                  </select>
+                </div>
+                <!-- End:Gender -->
+
+                <!-- Start:Gender Validation -->
+                <div
+                  class="text-sm mt-2 text-danger text-bold"
+                  v-if="FormHelpTexts.genderText"
+                >
+                  {{ FormHelpTexts.genderText }}
+                </div>
+                <!-- End:Gender Validation-->
+
+                <!-- Start:Password -->
+                <div class="input-group input-group-static mt-3">
+                  <label class="text-primary">Password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    v-model="password"
+                    placeholder="********"
+                    @keyup="validatePassword"
+                  />
+                </div>
+                <!-- End:Password-->
+
+                <!-- Start:Password Validation -->
+                <div
+                  class="text-danger text-sm mt-2 text-bold"
+                  v-if="FormHelpTexts.passwordText"
+                >
+                  {{ FormHelpTexts.passwordText }}
+                </div>
+                <!-- End:Password Validation -->
+
+                <!-- Start:Confirm Password -->
+                <div class="input-group input-group-static mt-3">
+                  <label class="text-primary">Confirm Password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    v-model="confirmPassword"
+                    placeholder="********"
+                    @keyup="validateConfirmPassword"
+                  />
+                </div>
+                <!-- End:Confirm Password -->
+
+                <!-- Start:Confirm Validation -->
+                <div
+                  class="text-danger text-sm mt-2 text-bold"
+                  v-if="FormHelpTexts.confirmPasswordText"
+                >
+                  {{ FormHelpTexts.confirmPasswordText }}
+                </div>
+                <!-- End:Confirm Password Validation -->
+
+                <!-- Start:Action Button -->
+                <div class="text-center">
+                  <button
+                    type="submit"
+                    class="btn bg-gradient-primary w-100 my-4 mb-2"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+                <!-- End:Action Button -->
+
+                <!-- Start:Login Link -->
+                <p class="mt-4 text-sm text-center">
+                  Already have an account?
+                  <NuxtLink
+                    class="text-primary text-gradient font-weight-bold"
+                    to="/login"
+                    >Login</NuxtLink
+                  >
+                </p>
+                <!-- End:Login Link -->
+              </form>
+              <!-- End:Sign Up Form -->
+            </div>
           </div>
         </div>
       </div>
