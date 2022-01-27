c3415528ae7171203970e328236abda6fd35d352
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-27 16:06:48 +0530

    add pagination
    add pagination using the data coming from api
    updates the list of items on the page after deletion so that they always remain 10 in count
    if the itm on page is only 1 and its deleted, fetches currentPage - 1 page

--- pages/admin/contact-form/index.vue
+++ pages/admin/contact-form/index.vue
@@ -73,16 +73,122 @@
                   </tbody>
                 </table>
               </div>
+
+              <!-- Start:Pagination -->
+              <section>
+                <div class="container">
+                  <div class="row justify-space-between py-2">
+                    <div class="col-md-4 mx-auto">
+                      <ul class="pagination pagination-primary m-4">
+                        <!-- Start:Previous Page Button -->
+                        <li class="page-item cursor-pointer">
+                          <div
+                            class="page-link"
+                            aria-label="Previous"
+                            @click="toPrevious()"
+                          >
+                            <span aria-hidden="true"
+                              ><i
+                                class="fas fa-chevron-left"
+                                aria-hidden="true"
+                              ></i
+                            ></span>
+                          </div>
+                        </li>
+                        <!-- End:Previous Page Button -->
+
+                        <!-- Start:First Page -->
+                        <li
+                          class="page-item cursor-pointer"
+                          v-if="contactForms.pagination.currentPage > 2"
+                        >
+                          <div class="page-link" @click="toNumber(1)">1</div>
+                        </li>
+                        <!-- End:First Page -->
+
+                        <!-- Start:Start Dashes -->
+                        <li
+                          class="page-item"
+                          v-if="contactForms.pagination.currentPage != 1"
+                        >
+                          <div class="d-flex align-items-end m-2">....</div>
+                        </li>
+                        <!-- End:Start Dashes -->
+
+                        <!-- Start:Current Page -->
+                        <li class="page-item active">
+                          <div class="page-link text-white">
+                            {{ contactForms.pagination.currentPage }}
+                          </div>
+                        </li>
+                        <!-- End:Current Page -->
+
+                        <!-- Start:End Dashes -->
+                        <li
+                          class="page-item"
+                          v-if="
+                            contactForms.pagination.currentPage <
+                            contactForms.pagination.totalPages
+                          "
+                        >
+                          <div class="d-flex align-items-end m-2">....</div>
+                        </li>
+                        <!-- End:End Dashes -->
+
+                        <!-- Start:Last Page -->
+                        <li
+                          class="page-item cursor-pointer"
+                          v-if="
+                            contactForms.pagination.currentPage <
+                            contactForms.pagination.totalPages - 1
+                          "
+                        >
+                          <div
+                            class="page-link"
+                            @click="
+                              toNumber(contactForms.pagination.totalPages)
+                            "
+                          >
+                            {{ contactForms.pagination.totalPages }}
+                          </div>
+                        </li>
+                        <!-- End:Last Page -->
+
+                        <!-- Start:Next Page Button -->
+                        <li class="page-item cursor-pointer">
+                          <div
+                            class="page-link"
+                            aria-label="Next"
+                            @click="toNext()"
+                          >
+                            <span aria-hidden="true"
+                              ><i
+                                class="fas fa-chevron-right"
+                                aria-hidden="true"
+                              ></i
+                            ></span>
+                          </div>
+                        </li>
+                        <!-- End:Next Page Button -->
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <!-- End:Pagination -->
             </div>
           </div>
-        </div>
-        <div class="col-12">
-          <div class="border-bottom"></div>
-          <div class="d-flex justify-content-end mx-4 mt-4">
-            <NuxtLink class="btn btn-success" to="/admin/contact-form/add"
-              >Add Contact Form</NuxtLink
-            >
+
+          <!-- Start:Add Contact Form Button -->
+          <div class="col-12">
+            <div class="border-bottom"></div>
+            <div class="d-flex justify-content-end mx-4 mt-4">
+              <NuxtLink class="btn btn-success" to="/admin/contact-form/add"
+                >Add Contact Form</NuxtLink
+              >
+            </div>
           </div>
+          <!-- End:Add Contact Form Button -->
         </div>
       </div>
     </div>
@@ -93,12 +199,63 @@
 export default {
   layout: 'admin',
 
+  data() {
+    return {
+      contactForms: Object,
+    }
+  },
+
   async asyncData({ $axios, $config }) {
     const contactForms = await $axios.$get('/ContactForms')
     return { contactForms }
   },
 
   methods: {
+    // jumps to previous page
+    toPrevious() {
+      if (this.contactForms.pagination.hasPrevious == true) {
+        let prevPage = this.contactForms.pagination.currentPage - 1
+
+        this.getContacts(prevPage).then((r) => {
+          this.contactForms = r
+          this.toTop()
+        })
+      }
+    },
+
+    // jumps to next page
+    toNext() {
+      if (this.contactForms.pagination.hasNext == true) {
+        let nextPage = this.contactForms.pagination.currentPage + 1
+
+        this.getContacts(nextPage).then((r) => {
+          this.contactForms = r
+          this.toTop()
+        })
+      }
+    },
+
+    // jumps to a specific page
+    toNumber(pageNum) {
+      this.getContacts(pageNum).then((r) => {
+        this.contactForms = r
+        this.toTop()
+      })
+    },
+
+    // Scrolls to top
+    toTop() {
+      window.scrollTo(0, 0)
+    },
+
+    // fetch new data
+    async getContacts(pageNum) {
+      let res = await this.$axios.$get(`/ContactForms?PageNumber=${pageNum}`)
+
+      return res
+    },
+
+    // formats the date
     formattedDate(inputDate) {
       const myDate = new Date(inputDate)
       return `${myDate.toLocaleString('default', {
@@ -106,7 +263,7 @@ export default {
       })}, ${myDate.getDate()}-${myDate.getMonth() + 1}-${myDate.getFullYear()}`
     },
 
-    // Delete the Contact Form
+    // deletes the Contact Form
     deleteContactForm(id) {
       const customAlert = this.$swal.mixin({
         customClass: {
@@ -127,17 +284,40 @@ export default {
         })
         .then(async (result) => {
           if (result.isConfirmed) {
-            await this.$axios.$delete(`/ContactForms/${id}`)
+            await this.$axios
+              .$delete(`/ContactForms/${id}`)
+              .then((res) => {
+                if (res.status == 200) {
+                  this.$swal({
+                    icon: 'success',
+                    title: 'Item Deleted',
+                    text: res.message,
+                  }).then(() => {
+                    this.contactForms.forms = this.contactForms.forms.filter(
+                      (item) => item.id != id
+                    )
 
-            this.$swal.fire({
-              icon: 'success',
-              title: 'Item Deleted',
-              text: 'Item Deleted Successfully',
-            })
+                    let currentPage
 
-            this.contactForms.forms = this.contactForms.forms.filter(
-              (item) => item.id != id
-            )
+                    if (this.contactForms.forms.length == 0) {
+                      currentPage = this.contactForms.pagination.currentPage - 1
+                    } else {
+                      currentPage = this.contactForms.pagination.currentPage
+                    }
+
+                    this.getContacts(currentPage).then((r) => {
+                      this.contactForms = r
+                    })
+                  })
+                }
+              })
+              .catch((err) => {
+                this.$swal({
+                  icon: 'error',
+                  title: 'Error',
+                  text: 'Unable to Delete Contact Form',
+                })
+              })
           }
         })
     },
