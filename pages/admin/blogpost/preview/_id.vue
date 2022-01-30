b59d2edb26b5d819d39e5b2e2c53347d3ef89552
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-30 16:59:36 +0530

    update blogpost view

--- pages/admin/blogpost/preview/_id.vue
+++ pages/admin/blogpost/preview/_id.vue
@@ -10,7 +10,7 @@
             <!-- Post -->
             <div class="container py-1">
               <div class="px-lg-6 px-xl-7 px-xxl-8">
-                <!-- post cover image  -->
+                <!-- post Featured image  -->
                 <div class="h-100 w-100 ratio ratio-16x9 my-3 my-lg-4">
                   <img
                     class="cover-img border-radius-2xl shadow-lg"
@@ -18,34 +18,38 @@
                   />
                 </div>
 
-                <!-- Date -->
-                <p class="text-center pt-4">
-                  <span class="badge bg-gradient-success rounded-pill"
-                    >Published</span
-                  >
-                  &nbsp; {{ getDatePosted() }}
-                </p>
-
                 <!-- title -->
                 <h1 class="text-center my-3">{{ post.title }}</h1>
+
+                <div>
+                  <p class="text-center">
+                    By<span class="mx-2 text-bold text-primary"
+                      >{{ author.user.firstName }}
+                      {{ author.user.lastName }} </span
+                    >on<span class="mx-2 text-bold text-info">{{
+                      getDatePosted()
+                    }}</span>
+                  </p>
+                </div>
 
                 <!-- category -->
                 <div class="d-flex justify-content-center">
                   <span
-                    class="badge rounded-pill bg-gradient-primary text-lg"
+                    class="badge rounded-pill bg-gradient-primary text-md"
                     >{{ post.category.name }}</span
                   >
                 </div>
 
-                <!-- Todo: Author -->
-
+                <div class="border-top mt-4"></div>
                 <!-- content -->
                 <div v-html="post.description" class="py-3 px-lg-5"></div>
 
+                <div class="border-top mb-4"></div>
+
                 <!-- Quick Action -->
-                <div class="d-flex justify-content-center">
+                <div class="d-flex justify-content-end">
                   <NuxtLink
-                    class="btn btn-lg btn-success rounded-pill"
+                    class="btn btn-lg bg-gradient-success"
                     :to="`/admin/blogpost/edit/${post.id}`"
                   >
                     <i class="fas fa-edit"></i>
@@ -68,7 +72,8 @@ export default {
 
   async asyncData({ $axios, $config, params }) {
     const post = await $axios.$get(`/BlogPosts/${params.id}`)
-    return { post }
+    const author = await $axios.$get(`/User/single/${post.authorId}`)
+    return { post, author }
   },
 
   methods: {
