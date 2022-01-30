23c5b5885f3bed720d8482758ebffe234153fd2a
Anmol Sharma <64188575+coder-anmol@users.noreply.github.com>
2022-01-30 20:23:03 +0530

    pagination component created and implemented in blog page, completed #32 (#34)
    * pagination component created and implemented in blog page
    
    * update comments
    
    Co-authored-by: Hardeep Kumar <hardeepkumar1815@gmail.com>

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -13,78 +13,95 @@
       <!-- End:Full size post -->
 
       <!-- Start:Card size post -->
-      <div class="col-lg-4 col-md-6" v-for="post in cardPosts">
+      <div class="col-lg-4 col-md-6" v-for="(post, i) in cardPosts" :key="i">
         <LazyBlogPostCard :post="post" />
       </div>
       <!-- End:Card size post -->
     </div>
     <!-- End:Latest Posts -->
 
-    <!-- Start:Pagination -->
-    <section>
-      <div class="container">
-        <div class="row justify-space-between py-2">
-          <div class="col-lg-4 mx-auto">
-            <ul class="pagination pagination-primary m-4">
-              <li class="page-item">
-                <a class="page-link" href="javascript:;" aria-label="Previous">
-                  <span aria-hidden="true"
-                    ><i class="fas fa-chevron-left" aria-hidden="true"></i
-                  ></span>
-                </a>
-              </li>
-              <li class="page-item active">
-                <a class="page-link" href="javascript:;">1</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:;">2</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:;">3</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:;">4</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:;">5</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:;" aria-label="Next">
-                  <span aria-hidden="true"
-                    ><i class="fas fa-chevron-right" aria-hidden="true"></i
-                  ></span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- End:Pagination -->
+    <!-- Pagination Start-->
+    <Pagination
+      :pageSize="this.pageSize"
+      :pagination="this.pagination"
+      :pages="this.pages"
+      @getSelectedPage="selectedPage($event)"
+      @getNextPage="nextPage"
+      @getPreviousPage="previousPage"
+      @getFirstPage="firstPage"
+      @getLastPage="lastPage"
+    />
+    <!-- Pagination End -->
   </div>
 </template>
 
 <script>
+import Pagination from '~/components/Pagination.vue'
 export default {
+  components: {
+    Pagination,
+  },
   data() {
     return {
       fullPost: '',
       cardPosts: [],
+      pageSize: 10,
+      pagination: {},
+      pages: [],
     }
   },
 
+  methods: {
+    getData(pageNumber) {
+      const blogPosts = this.$axios.$get(
+        `/BlogPosts?PageNumber=${pageNumber}&PageSize=${this.pageSize}`
+      )
+
+      blogPosts
+        .then((res) => {
+          this.fullPost = res.posts.slice(0, 1)[0]
+          this.cardPosts = res.posts.slice(1)
+          this.pagination = res.pagination
+        })
+        // Scroll to top on page change
+        .then(() => {
+          setTimeout(() => {
+            window.scroll(0, 0)
+          }, 0)
+        })
+    },
+    firstPage() {
+      this.getData(1)
+    },
+    lastPage() {
+      this.getData(this.pagination.totalPages)
+    },
+    nextPage() {
+      this.getData(this.pagination.currentPage + 1)
+    },
+    previousPage() {
+      this.getData(this.pagination.currentPage - 1)
+    },
+    selectedPage(pageNumber) {
+      this.getData(pageNumber)
+    },
+  },
   mounted() {
-    const blogPosts = this.$axios.$get('/BlogPosts')
+    const blogPosts = this.$axios.$get(`/BlogPosts?PageSize=${this.pageSize}`)
 
     blogPosts.then((res) => {
       this.fullPost = res.posts.slice(0, 1)[0]
       this.cardPosts = res.posts.slice(1)
+      this.pagination = res.pagination
+      this.pages = [...Array(this.pagination.totalPages).keys()].map(
+        (page) => page + 1
+      )
     })
   },
 }
 </script>
 
-<style>
+<style scoped>
 .cover-img {
   object-fit: cover;
 }
