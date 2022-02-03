dad7dc729d10eaf84b0ac903c786190db5b2ddfd
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-03 13:01:28 +0530

    merge Contact form (#42)
    * connect form to backend
    
    * add comments to explain the code

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,15 +5,10 @@
         <div class="col-lg-12 mx-auto">
           <div class="card">
             <div class="row">
+              <!-- Start:Information -->
               <div class="col-lg-4 d-flex">
                 <div
-                  class="
-                    bg-gradient-secondary
-                    mt-n5
-                    mb-3
-                    ms-lg-3
-                    border-radius-md
-                  "
+                  class="bg-gradient-secondary mt-n5 mb-3 ms-lg-3 border-radius-md"
                 >
                   <div class="card-body p-5 position-relative">
                     <h2 class="text-white">Contact Information</h2>
@@ -40,42 +35,22 @@
                     </div>
                     <div class="mt-4">
                       <a
-                        class="
-                          btn btn-icon-only btn-link
-                          text-white
-                          btn-lg
-                          mb-0
-                        "
+                        class="btn btn-icon-only btn-link text-white btn-lg mb-0"
                       >
                         <i class="fab fa-facebook"></i>
                       </a>
                       <a
-                        class="
-                          btn btn-icon-only btn-link
-                          text-white
-                          btn-lg
-                          mb-0
-                        "
+                        class="btn btn-icon-only btn-link text-white btn-lg mb-0"
                       >
                         <i class="fab fa-twitter"></i>
                       </a>
                       <a
-                        class="
-                          btn btn-icon-only btn-link
-                          text-white
-                          btn-lg
-                          mb-0
-                        "
+                        class="btn btn-icon-only btn-link text-white btn-lg mb-0"
                       >
                         <i class="fab fa-dribbble"></i>
                       </a>
                       <a
-                        class="
-                          btn btn-icon-only btn-link
-                          text-white
-                          btn-lg
-                          mb-0
-                        "
+                        class="btn btn-icon-only btn-link text-white btn-lg mb-0"
                       >
                         <i class="fab fa-instagram"></i>
                       </a>
@@ -83,10 +58,18 @@
                   </div>
                 </div>
               </div>
+              <!-- End:Information -->
+
+              <!-- Start:Contact Form -->
               <div class="col-lg-8">
-                <form id="contact-form" method="post" autocomplete="off">
+                <form
+                  id="contact-form"
+                  autocomplete="off"
+                  v-on:submit.prevent="addContactForm"
+                >
                   <div class="card-body position-relative">
                     <div class="row mt-4">
+                      <!-- Start:Full Name -->
                       <div class="col-md-6">
                         <div class="input-group input-group-static my-4">
                           <label class="text-primary">Full Name</label>
@@ -94,9 +77,14 @@
                             class="form-control"
                             aria-label="Full Name..."
                             type="text"
+                            placeholder="John Doe"
+                            v-model="fullName"
                           />
                         </div>
                       </div>
+                      <!-- End:Full Name -->
+
+                      <!-- Start:Email -->
                       <div class="col-md-6 ps-2">
                         <div class="input-group input-group-static my-4">
                           <label class="text-primary">Email</label>
@@ -104,11 +92,15 @@
                             type="email"
                             class="form-control"
                             aria-label="Email..."
+                            placeholder="johndoe@mail.com"
+                            v-model="email"
                           />
                         </div>
                       </div>
+                      <!-- End:Email -->
                     </div>
 
+                    <!-- Start:Message -->
                     <div class="input-group input-group-static mt-4">
                       <label class="text-primary">Your message</label>
                       <textarea
@@ -116,19 +108,26 @@
                         class="form-control"
                         id="message"
                         rows="6"
+                        placeholder="I am interested in..."
+                        v-model="message"
                       ></textarea>
                     </div>
+                    <!-- End:Message -->
+
+                    <!-- Start:Action -->
                     <div class="text-end mt-4">
                       <button
-                        type="button"
+                        type="submit"
                         class="btn bg-gradient-secondary mb-0"
                       >
                         Send message
                       </button>
                     </div>
+                    <!-- End:Action -->
                   </div>
                 </form>
               </div>
+              <!-- End:Contact Form -->
             </div>
           </div>
         </div>
@@ -138,7 +137,122 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      fullName: '',
+      email: '',
+      message: '',
+
+      // for error handling
+      FormHelpTexts: {
+        fullName: '',
+        email: '',
+        message: '',
+      },
+    }
+  },
+
+  methods: {
+    // for email validation
+    validateEmail() {
+      var re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (!re.test(this.email)) {
+        this.FormHelpTexts.email =
+          '<strong>Email</strong> must be a valid Email Address.<br/>'
+      } else {
+        this.FormHelpTexts.email = ''
+      }
+    },
+
+    // for Full Name validation
+    validateFullName() {
+      if (this.fullName.length < 3) {
+        this.FormHelpTexts.fullName =
+          '<strong>Full Name</strong> must be atleast 3 Characters long.<br/>'
+      } else {
+        this.FormHelpTexts.fullName = ''
+      }
+    },
+
+    // for Message validation
+    validateMessage() {
+      if (this.message.length < 20) {
+        this.FormHelpTexts.message =
+          '<strong>Message</strong> must be atleast 20 Characters long'
+      } else {
+        this.FormHelpTexts.message = ''
+      }
+    },
+
+    // Validate data
+    async addContactForm() {
+      this.validateFullName()
+      this.validateEmail()
+      this.validateMessage()
+
+      if (
+        this.FormHelpTexts.fullName == '' &&
+        this.FormHelpTexts.email == '' &&
+        this.FormHelpTexts.message == ''
+      ) {
+        this.addForm()
+      } else {
+        this.$swal({
+          title: 'Validation Errors!',
+          icon: 'error',
+          type: 'error',
+          html: `${this.FormHelpTexts.fullName}${this.FormHelpTexts.email}${this.FormHelpTexts.message}`,
+          confirmButtonText: 'Fix',
+        })
+      }
+    },
+
+    // Send data to Backend
+    async addForm() {
+      const data = {
+        fullName: this.fullName,
+        email: this.email,
+        message: this.message,
+      }
+
+      await this.$axios
+        .$post('/ContactForms', data)
+        .then((res) => {
+          if (res.status == 201) {
+            this.$swal({
+              title: 'Success!',
+              icon: 'success',
+              type: 'success',
+              html: `Message sent Successfully!<br/>We'll get back to you soon.`,
+              confirmButtonText: 'OK',
+            })
+            this.fullName = ''
+            this.email = ''
+            this.message = ''
+          }
+        })
+        .catch((err) => {
+          let msg
+
+          try {
+            msg = res.message
+          } catch (error) {
+            msg = 'Unable to Send Message.<br/>Please Try Again Later.'
+          }
+
+          this.$swal({
+            title: 'Error!',
+            html: `${msg}`,
+            type: 'error',
+            icon: 'error',
+            showConfirmButton: true,
+          })
+        })
+    },
+  },
+}
 </script>
 
 <style scoped></style>
