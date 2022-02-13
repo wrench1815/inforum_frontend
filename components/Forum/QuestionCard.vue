246994a9296222d636d0c9cbde601d9d57937fc4
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-13 17:06:29 +0530

    connect question with backend

--- components/Forum/QuestionCard.vue
+++ components/Forum/QuestionCard.vue
@@ -1,101 +1,90 @@
 <template>
   <!-- card -->
-  <div class="border border-radius-2xl bg-white mb-3 shadow-lg">
-    <!-- card top -->
-    <div class="p-3 border-bottom">
-      <div class="d-flex justify-content-start gap-3">
-        <!-- profile image -->
-        <span class="avatar avatar-md rounded-circle shadow-card">
-          <img src="https://i.pravatar.cc/100" alt="avatar" />
-        </span>
+  <div>
+    <div v-if="loading">
+      <h3 class="text-center">Loading.....</h3>
+    </div>
+    <div
+      class="border border-radius-2xl bg-white mb-3 shadow-lg"
+      v-if="!loading"
+    >
+      <!-- card top -->
+      <div class="p-3 border-bottom">
+        <div class="d-flex justify-content-start gap-3">
+          <!-- Start:Profile image -->
+          <span class="avatar avatar-md rounded-circle shadow-card">
+            <img
+              class="avatar avatar-md rounded-circle img-fit"
+              :src="author.profileImage"
+              alt="avatar"
+            />
+          </span>
+          <!-- End:Profile image -->
 
-        <span class="d-flex flex-column justify-content-center custom-gap">
-          <!-- name -->
-          <span class="text-bold text-md text-dark"> John Doe </span>
+          <span class="d-flex flex-column justify-content-center custom-gap">
+            <!-- name -->
+            <span class="text-bold text-md text-dark">{{
+              author.firstName + ' ' + author.lastName
+            }}</span>
 
-          <!-- date-uploaded -->
-          <span class="text-xs text-dark">{{ query.datePosted }}</span>
-        </span>
+            <!-- date-uploaded -->
+            <span class="text-xs text-dark">{{
+              dateFormatted(query.datePosted)
+            }}</span>
+          </span>
+        </div>
       </div>
-    </div>
 
-    <!-- card middle -->
-    <div class="p-3">
-      <!-- Start:Query Title -->
-      <NuxtLink to="/forum">
-        <h3 class="link-primary">{{ query.title }}</h3>
-      </NuxtLink>
-      <!-- End:Query Title -->
-      <p class="custom-paragraph">
-        {{ query.description }}
-      </p>
-      <p
-        class="bg-secondary text-white rounded-3 mb-0 text-sm px-2 py-1 d-inline-block"
-      >
-        {{ query.category.name }}
-      </p>
-    </div>
+      <!-- card middle -->
+      <div class="p-3">
+        <!-- Start:Query Title -->
+        <NuxtLink to="/forum">
+          <h3 class="link-primary">{{ query.title }}</h3>
+        </NuxtLink>
+        <!-- End:Query Title -->
 
-    <!-- card bottom -->
-    <div class="p-3 d-flex justify-content-start gap-5 border-top">
-      <!-- votes -->
-      <button
-        type="button"
-        class="btn bg-gradient-white w-auto m-0 p-0 text-sm"
-      >
-        <i class="fa fa-thumbs-up" />
-        10 Votes
-      </button>
+        <!-- Start:Query Description -->
+        <p class="custom-paragraph">
+          {{ query.description }}
+        </p>
+        <!-- End:Query Description -->
 
-      <!-- actions-->
-      <button
-        type="button"
-        class="btn bg-gradient-white w-auto m-0 p-0 text-sm"
-      >
-        <i class="fa fa-comment" />
-        5 Answers
-      </button>
-    </div>
+        <!-- Start:Query Category -->
+        <p
+          class="bg-secondary text-white rounded-3 mb-0 text-sm px-2 py-1 d-inline-block"
+        >
+          {{ query.category.name }}
+        </p>
+        <!-- End:Query Category -->
+      </div>
 
-    <!-- Comments -->
-    <div v-show="isCommentsVisible">
-      <!-- Create Comment -->
-      <CommentBox />
+      <!-- card bottom -->
+      <div class="p-3 d-flex justify-content-start gap-5 border-top">
+        <!-- votes -->
+        <button
+          type="button"
+          class="btn bg-gradient-white w-auto m-0 p-0 text-sm"
+        >
+          <i class="fa fa-thumbs-up" />
+          10 Votes
+        </button>
 
-      <!-- Comments Meta Data -->
-      <CommentHeading />
-
-      <!-- Actual Comments -->
-      <template v-if="comments.length">
-        <div>
-          <Comment
-            v-for="comment in comments"
-            :key="comment.id"
-            :content="comment.content"
-          />
-        </div>
-      </template>
-      <template v-else>
-        <div class="p-3">
-          <p class="text-center text-capitalize">No comments yet</p>
-        </div>
-      </template>
+        <!-- actions-->
+        <button
+          type="button"
+          class="btn bg-gradient-white w-auto m-0 p-0 text-sm"
+        >
+          <i class="fa fa-comment" />
+          5 Answers
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import CommentHeading from '~/components/Forum/CommentHeading.vue'
-import Comment from '~/components/Forum/Comment.vue'
-import CommentBox from '~/components/Forum/CommentBox.vue'
 export default {
   name: 'QuestionCard',
-
-  components: {
-    CommentHeading,
-    Comment,
-    CommentBox,
-  },
 
   props: {
     query: {
@@ -106,31 +95,29 @@ export default {
 
   data() {
     return {
-      isCommentsVisible: false,
-      comments: [
-        {
-          id: 1,
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit modi obcaecati id atque. Explicabo fugit quide delectus consectetur! ',
-        },
-        {
-          id: 2,
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit modi obcaecati id atque. Explicabo fugit quide delectus consectetur! ',
-        },
-        {
-          id: 3,
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit modi obcaecati id atque. Explicabo fugit quide delectus consectetur! ',
-        },
-      ],
+      loading: true,
+      author: Object,
     }
   },
 
   methods: {
-    showComments() {
-      this.isCommentsVisible = !this.isCommentsVisible
+    dateFormatted(dat) {
+      const myDate = new Date(dat)
+      return `${myDate.toLocaleString('default', {
+        weekday: 'short',
+      })}, ${myDate.getDate()}-${myDate.getMonth() + 1}-${myDate.getFullYear()}`
     },
+  },
+
+  mounted() {
+    this.$axios
+      .$get(`User/single/${this.query.authorId}`)
+      .then((res) => {
+        this.author = res.user
+      })
+      .then(() => {
+        this.loading = false
+      })
   },
 }
 </script>
@@ -145,5 +132,10 @@ export default {
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 3;
   overflow: hidden;
+}
+
+.img-fit {
+  object-fit: cover;
+  object-position: center;
 }
 </style>
