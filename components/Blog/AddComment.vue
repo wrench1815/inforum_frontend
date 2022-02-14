387581b79c13f88f3022c5fd6feda66094f00b52
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-14 12:11:50 +0530

    add authenticated condition

--- components/Blog/AddComment.vue
+++ components/Blog/AddComment.vue
@@ -46,38 +46,49 @@ export default {
     addComment() {
       let commentLength = this.commentInput.length
 
-      if (commentLength >= 3) {
-        const commentData = {
-          description: this.commentInput,
-          postId: this.postId,
-          userId: this.loggedInUser.id,
-        }
+      if (this.isAuthenticated) {
+        if (commentLength >= 3) {
+          const commentData = {
+            description: this.commentInput,
+            postId: this.postId,
+            userId: this.loggedInUser.id,
+          }
 
-        this.$axios
-          .$post(`/Comments`, commentData)
-          .then((res) => {
-            if (res.status == 201) {
-              // emit event to parent component on success
-              this.$emit('comment-added')
+          this.$axios
+            .$post(`/Comments`, commentData)
+            .then((res) => {
+              if (res.status == 201) {
+                // emit event to parent component on success
+                this.$emit('comment-added')
 
-              let timerInterval
+                let timerInterval
 
-              this.$swal({
-                title: 'Success!',
-                html: 'Thank you for leaving a comment!',
-                type: 'success',
-                icon: 'success',
-                timer: 2000,
-                showConfirmButton: false,
-                timerProgressBar: true,
-                didOpen: () => {
-                  this.$swal.showLoading()
-                },
-                willClose: () => {
-                  clearInterval(timerInterval)
-                },
-              })
-            } else {
+                this.$swal({
+                  title: 'Success!',
+                  html: 'Thank you for leaving a comment!',
+                  type: 'success',
+                  icon: 'success',
+                  timer: 2000,
+                  showConfirmButton: false,
+                  timerProgressBar: true,
+                  didOpen: () => {
+                    this.$swal.showLoading()
+                  },
+                  willClose: () => {
+                    clearInterval(timerInterval)
+                  },
+                })
+              } else {
+                this.$swal({
+                  title: 'Error!',
+                  html: 'Something went wrong!<br>Please try again.',
+                  type: 'error',
+                  icon: 'error',
+                  showConfirmButton: true,
+                })
+              }
+            })
+            .catch((err) => {
               this.$swal({
                 title: 'Error!',
                 html: 'Something went wrong!<br>Please try again.',
@@ -85,24 +96,34 @@ export default {
                 icon: 'error',
                 showConfirmButton: true,
               })
-            }
-          })
-          .catch((err) => {
-            this.$swal({
-              title: 'Error!',
-              html: 'Something went wrong!<br>Please try again.',
-              type: 'error',
-              icon: 'error',
-              showConfirmButton: true,
             })
+        } else {
+          this.$swal({
+            title: 'Too Short!',
+            html: 'Comment must be at least 3 characters long!',
+            type: 'info',
+            icon: 'info',
+            showConfirmButton: true,
           })
+        }
       } else {
         this.$swal({
-          title: 'Too Short!',
-          html: 'Comment must be at least 3 characters long!',
-          type: 'info',
-          icon: 'info',
-          showConfirmButton: true,
+          title: 'You need to be logged in to Comment',
+          text: 'Please login or Signup to Comment',
+          type: 'warning',
+          showCancelButton: true,
+          showCloseButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Login',
+          cancelButtonText: 'Signup',
+        }).then((result) => {
+          console.log(result)
+          if (result.isConfirmed) {
+            this.$router.push('/login')
+          } else if (result.isDismissed && result.dismiss === 'cancel') {
+            this.$router.push('/signup')
+          }
         })
       }
     },
