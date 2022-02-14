d75acf92c2a0e716ea07772f6f8fda04a32f1898
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-14 20:21:37 +0530

    add profile in nav
    restrict access for adding query

--- components/Forum/TopNav.vue
+++ components/Forum/TopNav.vue
@@ -8,17 +8,100 @@
           <img src="@/assets/icon.jpg" width="25rem" alt="logo" class="mb-1" />
           <span>Inforum</span>
         </NuxtLink>
-        <div><i class="fas fa-search fs-5"></i></div>
-        <div><i class="fas fa-user-astronaut fs-5"></i></div>
+        <!-- <div><i class="fas fa-search fs-5"></i></div> -->
+        <div>
+          <!-- <i class="fas fa-user-astronaut fs-5"></i> -->
+          <div class="dropdown">
+            <div
+              class="dropdown-toggle"
+              type="button"
+              id="AdminProfileDrop"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                v-if="!isAuthenticated"
+                class="avatar avatar-sm img-fit"
+                :src="profileImage"
+                alt=""
+              />
+              <img
+                v-if="isAuthenticated"
+                class="avatar avatar-sm img-fit"
+                :src="loggedInUser.profileImage"
+                alt=""
+              />
+            </div>
+            <ul
+              v-if="isAuthenticated"
+              class="dropdown-menu dropdown-menu-end dropdown-menu-sm-start"
+              aria-labelledby="AdminProfileDrop"
+            >
+              <li>
+                <NuxtLink class="dropdown-item text-dark" to="/profile"
+                  >Profile</NuxtLink
+                >
+              </li>
+              <li>
+                <NuxtLink class="dropdown-item text-dark" to="/admin"
+                  >Dash</NuxtLink
+                >
+              </li>
+              <li>
+                <div class="dropdown-divider border-dark opacity-4"></div>
+              </li>
+              <li>
+                <div class="dropdown-item text-dark">
+                  <NuxtLink class="btn btn-sm btn-danger m-0" to="/logout"
+                    >Logout<i class="ms-2 fas fa-sign-out-alt text-xs"></i
+                  ></NuxtLink>
+                </div>
+              </li>
+            </ul>
+            <ul
+              v-if="!isAuthenticated"
+              class="dropdown-menu dropdown-menu-end dropdown-menu-sm-start"
+              aria-labelledby="AdminProfileDrop"
+            >
+              <li>
+                <NuxtLink class="dropdown-item text-dark" to="/login"
+                  >Login<i class="ms-2 fas fa-sign-in-alt"></i
+                ></NuxtLink>
+              </li>
+              <li>
+                <NuxtLink class="dropdown-item text-dark" to="/signup"
+                  >Sign Up<i class="ms-2 fas fa-user-plus"></i
+                ></NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'TopNav',
+
+  data() {
+    return {
+      profileImage: require('@/assets/images/img-1.jpg'),
+    }
+  },
+
+  computed: {
+    ...mapGetters(['isAuthenticated', 'loggedInUser', 'loggedInUserRole']),
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.img-fit {
+  object-position: center;
+  object-fit: cover;
+}
+</style>
