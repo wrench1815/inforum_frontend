df59b35c5e485f68cf4e2ceca5b5068efd008f08
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-14 20:02:23 +0530

    refactor layout for htnl content

--- components/Forum/CompleteQuery.vue
+++ components/Forum/CompleteQuery.vue
@@ -37,13 +37,11 @@
       <!-- card middle -->
       <div class="p-3">
         <!-- Start:Query Title -->
-        <h3>{{ query.title }}</h3>
+        <h1>{{ query.title }}</h1>
         <!-- End:Query Title -->
 
         <!-- Start:Query Description -->
-        <p>
-          {{ query.description }}
-        </p>
+        <div v-html="query.description"></div>
         <!-- End:Query Description -->
 
         <!-- Start:Query Category -->
