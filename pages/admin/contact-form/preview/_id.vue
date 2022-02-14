4e1a5f0094aea2b566d2cbe2962e28b97f743e38
coder-anmol <dev.anmolsharma@gmail.com>
2022-02-14 19:02:54 +0530

    contact model fixed

--- pages/admin/contact-form/preview/_id.vue
+++ pages/admin/contact-form/preview/_id.vue
@@ -12,28 +12,62 @@
 
       <!-- Start:loaded -->
       <div class="card p-4" v-if="!loading">
+        <div class="card-header">
+          <h2>Contact Info</h2>
+        </div>
+        <!-- testing -->
         <div class="row">
-          <div class="col-12">
-            <h1 class="mb-3">Contact</h1>
-          </div>
-          <div class="col-12 mb-3">
-            <h4 class="">
-              <span class="me-1">By</span>
-              <span class="text-primary mx-1">{{ contact.fullName }}</span
-              >(<span class="text-info mx-1">{{ contact.email }}</span
-              >) <span class="mx-1">on</span
-              ><span class="mx-1 text-primary">{{
-                formattedDate(contact.createdOn)
-              }}</span>
-            </h4>
-          </div>
-          <div class="col-md-3">
-            <h3 class="">Message</h3>
-          </div>
-          <div class="col-md-9">
-            <section class="d-flex justify-content-center">
-              {{ contact.message }}
-            </section>
+          <!-- change basic info -->
+          <div class="mt-3">
+            <div class="card-body pt-0">
+              <div class="border-top mt-n3 mb-3"></div>
+              <!-- Start:Full Name -->
+              <div class="row">
+                <div class="col-md-4 col-12 text-primary">Full Name</div>
+                <div class="col-md-8 col-12 text-dark">
+                  {{ contact.fullName }}
+                </div>
+              </div>
+              <!-- End:Full Name -->
+
+              <!-- Start: Message -->
+              <div class="row my-3">
+                <div class="col-md-4 col-12 text-primary">Message</div>
+                <div class="col-md-8 col-12 text-dark">
+                  {{ contact.message }}
+                </div>
+              </div>
+              <!-- End: Message -->
+
+              <!-- Start:Email -->
+              <div class="row my-3">
+                <div class="col-md-4 col-12 text-primary">Email</div>
+                <div class="col-md-8 col-12 text-dark">{{ contact.email }}</div>
+              </div>
+              <!-- End:Email -->
+
+              <!-- Start:Date Created -->
+              <div class="row my-3">
+                <div class="col-md-4 col-12 text-primary">Date Created</div>
+                <div class="col-md-8 col-12 text-dark">
+                  {{ formattedDate(contact.createdOn) }}
+                </div>
+              </div>
+              <!-- End:Date created -->
+
+              <div class="border-top"></div>
+
+              <!-- Quick Action -->
+              <div class="d-flex justify-content-end my-3">
+                <NuxtLink
+                  class="btn bg-gradient-success"
+                  :to="`/admin/contact-form/edit/${contact.id}`"
+                >
+                  <i class="fas fa-edit"></i>
+                  Edit
+                </NuxtLink>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -85,6 +119,8 @@ export default {
           icon: 'error',
           html: `${msg}`,
         })
+
+        this.$router.go(-1)
       })
   },
 }
