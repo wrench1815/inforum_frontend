c64a61ada8185a0ed8b6066ac59973ec731a2592
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-20 17:24:37 +0530

    add popular query section
    refactor

---
+++ components/Forum/PopularQuery.vue
@@ -0,0 +1,44 @@
+<template>
+  <NuxtLink :to="`/forum/query/${query.slug}`">
+    <div class="card p-2 my-3 border shadow-secondary">
+      <h6 class="m-0" :class="randomClass">{{ query.title }}</h6>
+    </div>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  name: 'PopularQuery',
+
+  props: {
+    query: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      loading: true,
+      popularQueries: {},
+      textcols: [
+        'text-primary',
+        'text-secondary',
+        'text-success',
+        'text-info',
+        'text-warning',
+        'text-danger',
+        'text-dark',
+      ],
+      randomClass: '',
+    }
+  },
+
+  mounted() {
+    this.randomClass =
+      this.textcols[Math.floor(Math.random() * this.textcols.length)]
+  },
+}
+</script>
+
+<style></style>
