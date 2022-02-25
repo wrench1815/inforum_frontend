8532911d2d9d8f8ef2208c89558c8a58caa93a6a
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-25 21:10:54 +0530

    update footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -22,68 +22,106 @@
 
           <div class="col-lg col-md-4" :class="linksClasses">
             <ul class="nav flex-column">
+              <!-- Start:Home Link -->
               <li class="nav-item mb-2">
                 <NuxtLink to="/" class="nav-link p-0 text-muted">Home</NuxtLink>
               </li>
-              <li class="nav-item mb-2">
-                <NuxtLink to="/about" class="nav-link p-0 text-muted"
-                  >About</NuxtLink
-                >
-              </li>
-              <li class="nav-item mb-2">
-                <NuxtLink to="/blog" class="nav-link p-0 text-muted"
-                  >Blog</NuxtLink
-                >
-              </li>
+              <!-- End:Home Link -->
+
+              <!-- Start:Contact Link -->
               <li class="nav-item mb-2">
                 <NuxtLink to="/contact" class="nav-link p-0 text-muted"
                   >Contact</NuxtLink
                 >
               </li>
+              <!-- End:Contact Link -->
+
+              <!-- Start:About Link -->
+              <li class="nav-item mb-2">
+                <NuxtLink to="/about" class="nav-link p-0 text-muted"
+                  >About</NuxtLink
+                >
+              </li>
+              <!-- End:About Link -->
+
+              <!-- Start:Privacy Policy Link -->
+              <li class="nav-item mb-2">
+                <NuxtLink to="/privacy-policy" class="nav-link p-0 text-muted"
+                  >Privacy Policy</NuxtLink
+                >
+              </li>
+              <!-- End:Privacy Policy Link -->
+            </ul>
+          </div>
+
+          <div class="col-lg col-md-4" :class="linksClasses">
+            <ul class="nav flex-column">
+              <!-- Start:Forum Link -->
+              <li class="nav-item mb-2">
+                <NuxtLink to="/forum" class="nav-link p-0 text-muted"
+                  >Forum</NuxtLink
+                >
+              </li>
+              <!-- End:Forum Link -->
+
+              <!-- Start:Forum Categories Link -->
+              <li class="nav-item mb-2">
+                <NuxtLink to="/forum/category" class="nav-link p-0 text-muted"
+                  >Forum Categories</NuxtLink
+                >
+              </li>
+              <!-- End:Forum Categories Link -->
+
+              <!-- Start:Forum Search Link -->
+              <li class="nav-item mb-2">
+                <NuxtLink to="/forum/search" class="nav-link p-0 text-muted"
+                  >Forum Search</NuxtLink
+                >
+              </li>
+              <!-- End:Forum Search Link -->
+
+              <!-- Start:Admin Link -->
+              <li class="nav-item mb-2">
+                <NuxtLink to="/admin" class="nav-link p-0 text-muted"
+                  >Admin</NuxtLink
+                >
+              </li>
+              <!-- End:Admin Link -->
             </ul>
           </div>
           <div class="col-lg col-md-4" :class="linksClasses">
             <ul class="nav flex-column">
+              <!-- Start:Dash Link -->
               <li class="nav-item mb-2">
-                <NuxtLink to="/" class="nav-link p-0 text-muted">Home</NuxtLink>
-              </li>
-              <li class="nav-item mb-2">
-                <NuxtLink to="/about" class="nav-link p-0 text-muted"
-                  >About</NuxtLink
+                <NuxtLink to="/dash" class="nav-link p-0 text-muted"
+                  >Dash</NuxtLink
                 >
               </li>
+              <!-- End:Dash Link -->
+
+              <!-- Start:Profile Link -->
               <li class="nav-item mb-2">
-                <NuxtLink to="/blog" class="nav-link p-0 text-muted"
-                  >Blog</NuxtLink
+                <NuxtLink to="/profile" class="nav-link p-0 text-muted"
+                  >Profile</NuxtLink
                 >
               </li>
+              <!-- End:Profile Link -->
+
+              <!-- Start:Login Link -->
               <li class="nav-item mb-2">
-                <NuxtLink to="/contact" class="nav-link p-0 text-muted"
-                  >Contact</NuxtLink
+                <NuxtLink to="/login" class="nav-link p-0 text-muted"
+                  >Login</NuxtLink
                 >
               </li>
-            </ul>
-          </div>
-          <div class="col-lg col-md-4" :class="linksClasses">
-            <ul class="nav flex-column">
+              <!-- End:Login Link -->
+
+              <!-- Start:Sign Up Link -->
               <li class="nav-item mb-2">
-                <NuxtLink to="/" class="nav-link p-0 text-muted">Home</NuxtLink>
-              </li>
-              <li class="nav-item mb-2">
-                <NuxtLink to="/about" class="nav-link p-0 text-muted"
-                  >About</NuxtLink
+                <NuxtLink to="/signup" class="nav-link p-0 text-muted"
+                  >Sign Up</NuxtLink
                 >
               </li>
-              <li class="nav-item mb-2">
-                <NuxtLink to="/blog" class="nav-link p-0 text-muted"
-                  >Blog</NuxtLink
-                >
-              </li>
-              <li class="nav-item mb-2">
-                <NuxtLink to="/contact" class="nav-link p-0 text-muted"
-                  >Contact</NuxtLink
-                >
-              </li>
+              <!-- End:Sign Up Link -->
             </ul>
           </div>
         </div>
